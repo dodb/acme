e4451d62f2d0363ff69368456f726266b63c758a
Dirk Op de Beeck <dirk.opdebeeck@gmail.com>
2011-01-20 21:07:17 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Acme::Application.config.secret_token = '99a913214391a4134fbbe9247298eac08639e31f82f75850b08fb59920a2c1d11e6e11905c9597843a1a6ea3cf560f9fb5f4bff6b954b14841e4c8acd4a31be2'
